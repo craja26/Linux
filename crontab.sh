d5b16a0fac70f20d66390b115faf8213e28e4cd2
Raja <craja26@gmail.com>
2020-01-21 12:17:01 +0100

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,14 @@
+##### Cron Scheduler ######
+find / -name *cron*
+-> check schedule
+crontab -l
+
+-> edit schedule
+crontab -e
+
+-> remove
+crontab -r
+
+
+-> check specific user schedules
+crontab -u username -l
