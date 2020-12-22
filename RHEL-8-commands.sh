0a98ee7cdab890e2576a92cf647115a7382a3235
Raja <craja26@gmail.com>
2020-12-22 22:39:31 +0100

    Update RHEL-8-commands.sh

--- RHEL-8-commands.sh
+++ RHEL-8-commands.sh
@@ -68,6 +68,7 @@ Check the commands history: # history
 If you want to re-execute one of the history command: # !5  (executing 5 command in the history)
 
 Open new terminal: ctrl+shift+t
+
 /********* distributed file syste ***********/
 #pwd 
 /root
@@ -126,8 +127,125 @@ At the time of installation / and /boot will be created first.
 	checking example:	# which useradd
 						# which fdisk
 /etc: All the configuration files stored inside the etc folder.
-/srv: container management.
+/srv: container management, www, .
 /tmp: store temporary files. It will automatically get deleted if it is not used from past 10 days.
 /var: frequentially changes data(example: mail server data, database, webserver)
 /usr: stored all the documentation.(example: frontend code, programs)
+
+/******** Create own files and directory ********/
+New terminal: ctrl+shift+t
+Create a directory: mkdir <directory name>
+
+Types of directory struncture:
+	1. Nested directory
+		/data
+		data1
+		data2
+	2. Childlevel directory
+		/world/asia/india
+	
+Example: 	# mkdir /data
+			# mkdir /data/{data1,data2}
+List out subdirectories:	# ls -lR /data
+							# tree /data
+
+Created subdirectory along with parent directory if parent is not available: 
+	# mkdir /world/asia/india -p
+If parent directory already existed, create a subdirectory: # mkdir -p /data/data3
+
+Test: create a nested directory like below:
+/redhat
+|-rhce
+|  |-sa3
+|-rhcsa
+   |-sa1
+   |-sa2
+   
+Answer(check it once you have time):	# mkdir -p /redhat/{rhce/sa3,rhcsa/{sa1,sa2}} 
+
+Test 2: Create nested directory like below:
+/linux
+|-atc
+|	|-advansible
+|	|-rhel
+|-mmc
+|	|-aws
+|	|-cka
+|	|-ocp
+|-ol
+	|-devops
+	|-op
+Answer (test it later on linux machine): mkdir -p /linux/{atc/{advansible,rhel},mmc/{aws,cka,ocp},ol/{devops,op}}
+	# tree /linux
+
+Remove a directory: # rm -rf <directory>
+	example: # rm -rf /linux/mmc/cks
+	If directory is empty you can use rmdir command.
+	example: # rmdir <directory>	
+reverse search: (control+r)
+
+/****** Create files *******/
+1. touch <filename>
+	- It always create an empty file.
+	-Example: touch linux
+	Create multiple files at a time: # touch coss(1..10).txt
+2. cat > <filename>
+	- cat > linux2
+hello world!
+Good morning
+
+save: wq!
+	- adding more contenct to a file.
+	- cat >> linux2
+	- ctrl+d save file after adding.
+	Note: > = overwritting, >> = appending
+	- We cannot delete, update using "cat" command.
+3. vi, vim, nano, gedit.
+
+
+/********* Copy and Move *********/
+Copy: cp
+	replicating backup of original data.
+Exapmle:
+	create a file: # touch apple
+	Add content: # cat > apple
+	Hi all
+	(ctrl+d) for save
+	Copy: # cp apple newapple
+	
+	If copy is creating a new file in the same directory, it only creates new file. If you are copying to a different directory then it will take backup and create new file.
+	copy specific three files to new destination: # cp coss{1.3}.txt /abc
+	
+	Example: cp -rvpf /abc /data
+Test: create 5 mp3 files inside /opt folder. copy a1.mp3 a2.mp3 to /data directory. copy /opt directory to linux
+Answer:	# touch /opt/a{1..5}.mp3 
+		# ls /opt --listing files
+		# cp -rvpf /opt/a{1,2}.mp3 /data
+		# ls -ld linux --(checking directory)
+		# cp -rvpf /opt/* /linux
+	
+Move: mv
+	-rename or cut and paste
+	Example:	# touch apple
+				# mv apple pineapple
+				# mv pineapple /opt
+				
+/**** find command ****/
+Find - to search in your entire system
+grep: search a particular word in a file and display that line.
+	- it is a one time search.
+
+Example: How to search a word "root" in /etc/passwd 
+	- # grep root /etc/passwd
+	Search with number: # grep -n -A 2 <search word> <filename>
+	search a line start with linux word: # grep ^linux <filename>
+	Search line starts with case incensitive word linux: # grep -i ^linux <filename>
+	Search lines excluding a word linux: # grep -v linux <filename>
+	Search word count for linux: # grep -ic linux <filename>
+	Search line end with "batch": # grep batch$ <filename>
+	Search number ranges 10 to 20: # 
+Question: Find a string 'ich' from "/usr/share/dict/words" and put it into /root/lines file.
+Answer: # grep 'ich' /usr/share/dict/words > /root/lines
+
+
 
